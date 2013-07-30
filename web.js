d4323b54461f2c14d289477d89904a273119ffb2
tron1991 <eco.helpservices@gmail.com>
2013-07-30 19:06:11 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,10 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+var htmlfile = "index.html";
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+var app = express.createServer(express.logger());
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.get('/', function(request, response){
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
